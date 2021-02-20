d336c4fb9222f72ec9c24c39582fa1e3e37fa33d
Nigina Daniyarova <>
2021-02-20 13:00:28 -0500

    Added fizzbuzz

--- fizzbuzz.cpp
+++ fizzbuzz.cpp
@@ -1,5 +1,5 @@
-// Name:
-// Date:
+// Name: Nigina Daniyarova
+// Date: 2/20/21
 
 #include <iostream>
 using namespace std;
@@ -10,7 +10,28 @@ int main(int argc, char** argv)
    cin >> X;
    cin >> Y;
    cin >> N;
+   cout << "\n";
 
-   // Your code here
+   for (int i = 1; i <= N; i++)
+   {
+   		if (i%X == 0 && i%Y == 0)
+   		{
+   			cout << "FizzBuzz\n";
+   		}
+   		if (i%X == 0 && i%Y != 0)
+   		{
+   			cout << "Fizz\n";
+   		}
+   		if (i%X != 0 && i%Y == 0)
+   		{
+   			cout << "Buzz\n";
+   		}
+   		if (i%X != 0 && i%Y !=0)
+   		{
+   			cout << i;
+   			cout << "\n";
+   		}
+
+   }
 }
 
